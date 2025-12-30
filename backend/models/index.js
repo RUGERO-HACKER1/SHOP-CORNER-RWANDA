63b893e05d0d2be6562924b98f6e52f08b65d98f
RUGERO-HACKER1 <fidelerugero100@gmail.com>
2025-12-30 02:40:54 -0800

    Syntax Error Fixed

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,26 +2,27 @@ const { Sequelize, DataTypes } = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
 
-    ?new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    logging: false,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-})
-    : new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
+const sequelize = process.env.DATABASE_URL
+    ? new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres',
-        logging: false
-    }
-);
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    })
+    : new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            dialect: 'postgres',
+            logging: false
+        }
+    );
 
 const User = sequelize.define('User', {
     name: { type: DataTypes.STRING, allowNull: false },
