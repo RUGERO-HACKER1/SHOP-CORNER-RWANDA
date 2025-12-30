177869d04a0ee6dc1f83b73da741a37add9bfea3
RUGERO-HACKER1 <fidelerugero100@gmail.com>
2025-12-30 02:36:56 -0800

    Database Configured for Neon

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,21 +2,26 @@ const { Sequelize, DataTypes } = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const sequelize = process.env.DATABASE_URL
-    ? new Sequelize(process.env.DATABASE_URL, {
+    ?new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+})
+    : new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
         dialect: 'postgres',
         logging: false
-    })
-    : new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: process.env.DB_HOST,
-            dialect: 'postgres',
-            logging: false
-        }
-    );
+    }
+);
 
 const User = sequelize.define('User', {
     name: { type: DataTypes.STRING, allowNull: false },
